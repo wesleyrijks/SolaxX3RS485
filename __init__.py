d5a0266dfb5eb13fcf9f49935729b00f2f891f46
wesleyrijks <86784698+wesleyrijks@users.noreply.github.com>
2023-05-14 11:20:38 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,21 +1,57 @@
-#!/usr/bin/python3
+from pymodbus.client.sync import ModbusSerialClient
+from pymodbus.constants import Defaults
+import json
 
-from twisted.internet import task
-from SolaxX3RS485 import SolaxX3RS485
+Defaults.UnitId = 1
 
-def outputActions(vals):
-    global outputs
-    if outputs is None:
-        return
+def join_msb_lsb(msb, lsb):
+    return (msb << 16) | lsb
 
-def inputActions(inputs):
-    for input in inputs:
-        input.fetch(outputActions)
+def fetch(port, baudrate, parity, stopbits, timeout):
+    client = ModbusSerialClient(method="rtu", port=port, baudrate=9600, parity="N", stopbits=1, timeout=1)
+
+    result = client.read_input_registers(0X400, 53)
+    
+    vals = {}
+    vals['Pv1 input voltage'] = result.getRegister(0) / 10
+    vals['Pv2 input voltage'] = result.getRegister(1) / 10
+    vals['Pv1 input current'] = result.getRegister(2) / 10
+    vals['Pv2 input current'] = result.getRegister(3) / 10
+    vals['Grid Voltage Phase 1'] = result.getRegister(4) / 10
+    vals['Grid Voltage Phase 2'] = result.getRegister(5) / 10
+    vals['Grid Voltage Phase 3'] = result.getRegister(6) / 10
+    vals['Grid Frequency Phase 1'] = result.getRegister(7) / 100
+    vals['Grid Frequency Phase 2'] = result.getRegister(8) / 100
+    vals['Grid Frequency Phase 3'] = result.getRegister(9) / 100
+    vals['Output Current Phase 1'] = result.getRegister(10) / 10
+    vals['Output Current Phase 2'] = result.getRegister(11) / 10
+    vals['Output Current Phase 3'] = result.getRegister(12) / 10
+    vals['Temperature'] = result.getRegister(13)
+    vals['Inverter Power'] = result.getRegister(14)
+    vals['RunMode'] = result.getRegister(15)
+    vals['Output Power Phase 1'] = result.getRegister(16)
+    vals['Output Power Phase 2'] = result.getRegister(17)
+    vals['Output Power Phase 3'] = result.getRegister(18)
+    vals['Total DC Power'] = result.getRegister(19)
+    vals['PV1 DC Power'] = result.getRegister(20)
+    vals['PV2 DC Power'] = result.getRegister(21)
+    vals['Fault value of Phase 1 Voltage'] = result.getRegister(22) / 10
+    vals['Fault value of Phase 2 Voltage'] = result.getRegister(23) / 10
+    vals['Fault value of Phase 3 Voltage'] = result.getRegister(24) / 10
+    vals['Fault value of Phase 1 Frequency'] = result.getRegister(25) / 100
+    vals['Fault value of Phase 2 Frequency'] = result.getRegister(26) / 100
+    vals['Fault value of Phase 3 Frequency'] = result.getRegister(27) / 100
+    vals['Fault value of Phase 1 DCI'] = result.getRegister(28) / 1000
+    vals['Fault value of Phase 2 DCI'] = result.getRegister(29) / 1000
+    vals['Fault value of Phase 3 DCI'] = result.getRegister(30) / 1000
+    vals['Fault value of PV1 Voltage'] = result.getRegister(31) / 10
+    vals['Fault value of PV2 Voltage'] = result.getRegister(32) / 10
+    vals['Fault value of Temperature'] = result.getRegister(33)
+    vals['Fault value of GFCI'] = result.getRegister(34) / 1000
+    vals['Total Yield'] = join_msb_lsb(result.getRegister(36), result.getRegister(35)) / 1000
+    vals['Yield Today'] = join_msb_lsb(result.getRegister(38), result.getRegister(37)) / 1000
+
+    print(json.dumps(vals))
 
 
-SolaxRS485Meters = []
-SolaxRS485Meters.append(SolaxX3RS485("/dev/ttyUSB0", 9600, "N", 1, 1))
-
-looperSolaxRS485 = task.LoopingCall(inputActions, SolaxRS485Meters)
-looperSolaxRS485.start(0)
-
+fetch("/dev/ttyUSB0", 9600, "N", 1, 1)
